b30761e37f79ce591ae692caf73ac772cb5aec0f
maxybum <maxybum@gmail.com>
2020-02-27 16:25:40 +0200

    Adde Task2
    Added Tesk2

--- RichTextEditorWpf/MainWindow.xaml.cs
+++ RichTextEditorWpf/MainWindow.xaml.cs
@@ -5,6 +5,11 @@ using System.Collections.Generic;
 using System.Linq;
 using RichTextEditorWpf.Services;
 using Unity;
+using System.Windows.Documents;
+using System.Windows.Controls.Primitives;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System;
 
 namespace RichTextEditorWpf
 {
@@ -18,10 +23,16 @@ namespace RichTextEditorWpf
         private ISaveService _saveService;
         private ILoadService _loadService;
         private IPrintService _printService;
-        
+        private IUrlToHyperlinkService _urlToHyperlinkService;
+
         public MainWindow()
         {
             InitializationTemplate();
+        }
+
+        private void ChangeAllUrlToHyperlink(object sender, RoutedEventArgs e)
+        {
+            _urlToHyperlinkService.UrlToHyperlinkCommand(mainRTB);
         }
 
         public void InitializationTemplate()
@@ -42,12 +53,14 @@ namespace RichTextEditorWpf
             container.RegisterType<ISaveService, SaveService>();
             container.RegisterType<ILoadService, LoadService>();
             container.RegisterType<IPrintService, PrintService>();
+            container.RegisterType<IUrlToHyperlinkService, UrlToHyperlinkService>();
 
             _imageService = container.Resolve<ImageService>();
             _fontService = container.Resolve<FontsService>();
             _saveService = container.Resolve<SaveService>();
             _loadService = container.Resolve<LoadService>();
             _printService = container.Resolve<PrintService>();
+            _urlToHyperlinkService = container.Resolve<UrlToHyperlinkService>();
         }
 
         private void InitiazeDefaultValues()
@@ -59,6 +72,11 @@ namespace RichTextEditorWpf
             var fontSizes = new List<double>() { 8, 9, 10, 11, 12, 14, 16, 18, 20, 22, 24, 26, 28, 36, 48, 72 };
             cmbFontSize.ItemsSource = fontSizes;
             cmbFontSize.SelectedItem = fontSizes.First();
+        }
+
+        private void rtbEditor_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            SelectionChangedCommand();
         }
 
         private void AddImage(object sender, RoutedEventArgs e)
@@ -89,6 +107,66 @@ namespace RichTextEditorWpf
         void LoadFile(object sender, RoutedEventArgs args)
         {
             _loadService.LoadCommand(mainRTB);
-        }      
+        }
+
+        private void SelectionChangedCommand()
+        {
+            object temp = mainRTB.Selection.GetPropertyValue(Inline.FontWeightProperty);
+            btnBold.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontWeights.Bold));
+            temp = mainRTB.Selection.GetPropertyValue(Inline.FontStyleProperty);
+            btnItalic.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(FontStyles.Italic));
+            temp = mainRTB.Selection.GetPropertyValue(Inline.TextDecorationsProperty);
+            btnUnderline.IsChecked = (temp != DependencyProperty.UnsetValue) && (temp.Equals(TextDecorations.Underline));
+
+            temp = mainRTB.Selection.GetPropertyValue(Inline.FontFamilyProperty);
+            cmbFontFamily.SelectedItem = temp;
+            temp = mainRTB.Selection.GetPropertyValue(Inline.FontSizeProperty);
+            cmbFontSize.Text = temp.ToString();
+
+            UpdateVisualState();
+        }
+
+        private void UpdateVisualState()
+        {
+            UpdateToggleButtonState();
+            UpdateSelectionListType();
+        }
+
+        private void UpdateSelectionListType()
+        {
+            Paragraph startParagraph = mainRTB.Selection.Start.Paragraph;
+            Paragraph endParagraph = mainRTB.Selection.End.Paragraph;
+            if (startParagraph != null && endParagraph != null && (startParagraph.Parent is ListItem) && (endParagraph.Parent is ListItem) && object.ReferenceEquals(((ListItem)startParagraph.Parent).List, ((ListItem)endParagraph.Parent).List))
+            {
+                TextMarkerStyle markerStyle = ((ListItem)startParagraph.Parent).List.MarkerStyle;
+                if (markerStyle == TextMarkerStyle.Disc) //bullets  
+                {
+                    _btnBullets.IsChecked = true;
+                }
+                else if (markerStyle == TextMarkerStyle.Decimal) //number  
+                {
+                    _btnNumbers.IsChecked = true;
+                }
+            }
+            else
+            {
+                _btnBullets.IsChecked = false;
+                _btnNumbers.IsChecked = false;
+            }
+        }
+
+        void UpdateItemCheckedState(ToggleButton button, DependencyProperty formattingProperty, object expectedValue)
+        {
+            object currentValue = mainRTB.Selection.GetPropertyValue(formattingProperty);
+            button.IsChecked = (currentValue == DependencyProperty.UnsetValue) ? false : currentValue != null && currentValue.Equals(expectedValue);
+        }
+
+        private void UpdateToggleButtonState()
+        {
+            UpdateItemCheckedState(_btnAlignLeft, Paragraph.TextAlignmentProperty, TextAlignment.Left);
+            UpdateItemCheckedState(_btnAlignCenter, Paragraph.TextAlignmentProperty, TextAlignment.Center);
+            UpdateItemCheckedState(_btnAlignRight, Paragraph.TextAlignmentProperty, TextAlignment.Right);
+            UpdateItemCheckedState(_btnAlignJustify, Paragraph.TextAlignmentProperty, TextAlignment.Right);
+        }
     }
 }
---
+++ RichTextEditorWpf/Services/UrlToHyperlinkService.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows.Controls;
+using System.Windows.Documents;
+using System;
+using System.Text.RegularExpressions;
+
+namespace RichTextEditorWpf.Services
+{
+    public class UrlToHyperlinkService : IUrlToHyperlinkService
+    {
+        private static readonly Regex UrlRegex = new Regex(@"(http|ftp|https):\/\/([\w\-_]+(?:(?:\.[\w\-_]+)+))([\w\-\.,@?^=%&amp;:/~\+#]*[\w\-\@?^=%&amp;/~\+#])?", RegexOptions.Compiled | RegexOptions.IgnoreCase);
+
+        public void UrlToHyperlinkCommand(RichTextBox mainRTB)
+        {
+            TextRange text = new TextRange(mainRTB.Document.ContentStart, mainRTB.Document.ContentEnd);
+            TextPointer current = text.Start.GetInsertionPosition(LogicalDirection.Forward);
+            while (current != null)
+            {
+                var textInRun = current.GetTextInRun(LogicalDirection.Forward);
+
+                var match = UrlRegex.Match(textInRun).Value;
+
+                if (!string.IsNullOrWhiteSpace(textInRun))
+                {
+                    if (!string.IsNullOrWhiteSpace(match))
+                    {
+                        int index = textInRun.IndexOf(match);
+                        if (index != -1)
+                        {
+                            var selectionStart = current.GetPositionAtOffset(index);
+                            var selectionEnd = selectionStart.GetPositionAtOffset(match.Length);
+                            var selection = new TextRange(selectionStart, selectionEnd);
+
+                            try
+                            {
+                                var hlink = new Hyperlink(selectionStart, selectionEnd);
+
+                                hlink.NavigateUri = new Uri(match);
+                            }
+                            catch (Exception ex)
+                            {
+                                //TODO: implement Logger
+                            }
+                        }
+                    }
+
+                }
+                current = current.GetNextContextPosition(LogicalDirection.Forward);
+            }
+        }
+    }
+
+    public interface IUrlToHyperlinkService
+    {
+        void UrlToHyperlinkCommand(RichTextBox mainRTB);
+    }
+}
